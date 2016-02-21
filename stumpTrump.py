df2dddaf74525b27ca1c28666672056c8545c9e9
aneeshdurg <durg2@illinois.edu>
2016-02-20 18:03:08 -0600

    FREEDOM

--- stumpTrump.py
+++ stumpTrump.py
@@ -2,7 +2,7 @@ import cv2
 import sys
 from random import randint
 import pygame
-
+from time import time
 pygame.init()
 SIZE = [1920, 1080]
 canvas = pygame.display.set_mode(SIZE)
@@ -11,7 +11,7 @@ pygame.display.set_caption("Motion Game")
 #nvas = canvas.convert()
 canvas.fill((255, 255, 255))
 
-trump = [None, None, None]
+trump = [None, None, None, None]
 alivetrump = [True, True, True]
 trump[0] = pygame.image.load("trump1.png").convert()
 trump[0].set_colorkey((255, 255, 255))
@@ -22,9 +22,16 @@ trump[1] = pygame.transform.scale(trump[1], (200, 200))
 trump[2] = pygame.image.load("trump3.png").convert()
 trump[2].set_colorkey((255, 255, 255))
 trump[2] = pygame.transform.scale(trump[2], (200, 200))
+trump[3] = pygame.image.load("trump_baby.png").convert()
+trump[3].set_colorkey((255, 255, 255))
+trump[3] = pygame.transform.scale(trump[3], (100, 150))
+
 
 if "-a" in sys.argv:
 	sys.argv=["-v", "-p"]
+level = 1
+if "-l" in sys.argv:
+    level = 10
 	
 cap = cv2.VideoCapture(0)
 size = int(cap.get(4)/2)
@@ -36,7 +43,6 @@ Previous = None
 baseLine = None
 aboveBL = True 
 tid = 0
-level = 1
 nextlvl = True
 hitsize = 200
 ghx, ghy = None, None
@@ -46,8 +52,19 @@ hit = [0, 0, 0]
 nextlvl = True 
 img=[]
 health = 100
+font = pygame.font.Font(None, 36)
+bossstrt = 0
 while True:
         canvas.fill((255, 255, 255))
+        if level == 10:
+            #print "BOSS WAVE"
+            if bossstrt==0:
+                bossstrt = time()
+            if time()-bossstrt<3:
+                text = font.render("DODGE", 1, (10, 10, 10))
+                textpos = text.get_rect()
+                textpos.centerx = canvas.get_rect().centerx
+                canvas.blit(text, textpos)
         pygame.draw.line(canvas, (0, 255, 0), (0, 0), (int(health*SIZE[0]/100), 0), 5)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -93,6 +110,10 @@ while True:
                 tpos.append([randint(50, width-100), 0])
                 img.append(randint(0, 2))
                 alivetrump.append(True)
+            if level == 10:
+                for i in xrange(level):
+                    img[i] = 3
+                    tpos[i][1] = -500*i
             nextlvl = False
         else:
             for i in xrange(level):
@@ -103,7 +124,7 @@ while True:
             #tpos[tid][0]-=5
             tpos[tid][1]+=5
             if alivetrump[tid]:
-                cv2.rectangle(frame, (tpos[tid][0], tpos[tid][1]), (tpos[tid][0]+100, tpos[tid][1]+100), (0, 0, 255), 2) 
+                cv2.rectangle(frame, (tpos[tid][0], tpos[tid][1]+100), (tpos[tid][0]+100, tpos[tid][1]+200), (0, 0, 255), 2) 
                 #scaledsize[tid] = [int(tsize[tid][0]*SIZE[0]/width), int(tsize[tid][1]*SIZE[1]/hieght)]
                 scaledpos[tid] = [int((width - tpos[tid][0])*SIZE[0]/width), int(tpos[tid][1]*SIZE[1]/hieght)]
                 #trump[tid] = pygame.transform.scale(trump[tid], (scaledsize[tid][0], scaledsize[tid][1]))
@@ -137,36 +158,65 @@ while True:
                 yavg+=(y+(h/2))
                 xavg+=(x+(w/2))
                 counter += 1
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        for tid in xrange(level):
-            if tpos[tid][1]>int(3*hieght/4):
+		#cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        if level!=10:
+            for tid in xrange(level):
+                if tpos[tid][1]>int(3*hieght/4):
+                    if alivetrump[tid]:
+                        print "you couldn't stump the trump!"
+                        health-=5
+                    if health<=0:
+                        print "you lose! Final score: "+str((level-1)*10)
+                        cv2.destroyAllWindows()
+                        pygame.display.quit()
+                        pygame.quit()
+                        exit()
+                    alivetrump[tid] = False
+        
+            for tid in xrange(level):
+                if hit[tid]>=10:
+                    print "hit"
+                    alivetrump[tid] = False
+                    hit[tid] = 0
+            remaining = False 
+        
+            for tid in xrange(level):
                 if alivetrump[tid]:
+                    remaining = True
+                    break
+        
+            if not remaining:
+                level+=1
+                nextlvl = True
+        else:
+            for tid in xrange(level):
+                if alivetrump[tid] and tpos[tid][1]>hieght:
+                    print "Nice dodge!"
+                    alivetrump[tid] = False
+            for tid in xrange(level):
+                if hit[tid]>=5:
                     print "you couldn't stump the trump!"
                     health-=5
+                    hit[tid] = 0
                 if health<=0:
-                    print "you lose! Final score: "+str(level*10)
+                    print "you lose! Final score: "+str((level-1)*10)
                     cv2.destroyAllWindows()
                     pygame.display.quit()
                     pygame.quit()
                     exit()
-                alivetrump[tid] = False
-                #trump[tid] = pygame.image.load("trump"+str(tid+1)+".png").convert()
-                #trump[tid].set_colorkey((255, 255, 255))
-                #tpos[tid] = [randint(50, width-100), 0]
-                #tsize[tid] = [100, 100]
-        for tid in xrange(level):
-            if hit[tid]>=10:
-                print "hit"
-                alivetrump[tid] = False
-                hit[tid] = 0
-        remaining = False 
-        for tid in xrange(level):
-            if alivetrump[tid]:
-                remaining = True
-                break
-        if not remaining:
-            level+=1
-            nextlvl = True
+            remaining = False 
+        
+            for tid in xrange(level):
+                if alivetrump[tid]:
+                    remaining = True
+                    break
+        
+            if not remaining:
+                print "You stumped the trump! Final score: 100"
+                cv2.destroyAllWindows()
+                pygame.display.quit()
+                pygame.quit()
+                exit()
         if mvLeft>=5:
             hx+=mvLeft
         if mvRight>=5:
@@ -179,8 +229,6 @@ while True:
         if counter>0:	
 	    yavg/=counter
             xavg/=counter
-            #cv2.line(frame, (0, yavg), (int(width), yavg), (255, 0, 0), 2)
-	    #cv2.line(frame, (xavg, 0), (xavg, int(hieght)), (255, 0, 0), 2)
 
         frame = cv2.flip(frame, 1)	
 	
@@ -188,7 +236,20 @@ while True:
 	    cv2.imshow("Detected motion", frame)
             #cv2.imshow("fd", frameDelta)	
 	Previous = Current
-        ghx = int((width-hx)*SIZE[0]/width)
-        ghy = int(hy*SIZE[1]/hieght)
-        pygame.draw.circle(canvas, (0, 255, 0), (ghx, ghy), 10)
+        if level==10:
+            ghx = int((width-hx)*SIZE[0]/width)
+            ghy = int(hieght-100)#baseLine#int(hy*SIZE[1]/hieght)
+            for tid in xrange(level):
+                print scaledpos
+                if ghx in range(scaledpos[tid][0], scaledpos[tid][0]+100):
+                    if ghy in range(scaledpos[tid][1], scaledpos[tid][1]+100):
+                        print "you couldn't stump the trump"
+                        health-=5
+                        if health<=0:
+                            print "you lose"
+                            cv2.destroyAllWindows()
+                            pygame.display.quit()
+                            pygame.quit()
+                            exit()
+            pygame.draw.circle(canvas, (255, 0, 0), (ghx, ghy), 10)
         pygame.display.flip()
